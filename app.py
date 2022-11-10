92c06a6e319b389e47a16b66960d401fb6bc002f
zdanukm <zdanuk_m@wp.pl>
2022-11-10 23:27:32 +0100

    added authentication, changed proj structure

--- app.py
+++ app.py
@@ -1,8 +1,65 @@
-from distutils.debug import DEBUG
-from flask import Flask,request, render_template
+from flask import Flask, render_template, url_for, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 import json
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.sqlite3')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+    # def __init__(self, username, password):
+    #         self.username = username
+    #         self.password = password
+    # def __repr__(self):
+    #     return f'<username {self.username}>'
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20)],render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Login')
+
 
 def read_words_from_file(file_path):
     word_file = file_path
@@ -10,33 +67,68 @@ def read_words_from_file(file_path):
     return words
 
 tab = read_words_from_file("words")
-# product_list = Product.objects.all()
-
-@app.route("/")
-def home():
-    return render_template("home.html")
 
 
-@app.route("/index")
-def index():
-    return render_template("index.html")
+@app.route("/products")
+def products():
+    return render_template("products.html")
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
+@app.route("/mainpage")
+def mainpage():
+    return render_template("mainpage.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+    
 @app.route("/search")
 def search():
     text = request.args['searchText']
     result = [c for c in tab if str(text).lower() in c.lower()]
     return json.dumps({"results":result})
 
-@app.route('/with-post', methods=["POST"])
-def with_post():
-    username = request.form.get("username", "nothing")
-    password = request.form.get("password", "nothing")
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-    return "Got [" + username + "] and [" + password + "]", 200
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
 
-if __name__ == "__main__":
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+        
+    return render_template('register.html', form=form)
+
+
+if __name__ == '__main__':
+    # with app.app_context():
+    #     db.create_all()
     app.run(debug=True)
+    
